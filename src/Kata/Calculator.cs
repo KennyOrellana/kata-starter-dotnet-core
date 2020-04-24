1f3b375ffb232f0666caed42eee4c3354510d759
Kenny Orellana <lorellana@todo1.ney>
2020-04-24 14:51:04 -0600

    Input many numbers

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Kata.Spec
 {
     public class Calculator
@@ -8,7 +10,19 @@ namespace Kata.Spec
             {
                 return 0;
             }
-                
+
+            if (number.Contains(","))
+            {
+                string[] numbers = number.Split(",");
+                if (numbers.Length == 2)
+                {
+                    int number1 = int.Parse(numbers[0]);
+                    int number2 = int.Parse(numbers[1]);
+                    
+                    return number1 + number2;
+                }
+            }
+            
             return int.Parse(number);
         }
     }
